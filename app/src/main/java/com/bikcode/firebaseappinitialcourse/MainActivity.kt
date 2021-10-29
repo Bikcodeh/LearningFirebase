04d23ec79a9972e719fa41aeb887a760ae164766
Victor Hoyos <bikcodeh@gmail.com>
2021-10-28 23:05:49 -0500

    implements basic crud for read, write and remove data from realtime database

--- app/src/main/java/com/bikcode/firebaseappinitialcourse/MainActivity.kt
+++ app/src/main/java/com/bikcode/firebaseappinitialcourse/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.bikcode.firebaseappinitialcourse
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bikcode.firebaseappinitialcourse.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityMainBinding
+    private lateinit var database: DatabaseReference
+    private lateinit var dataReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        database = Firebase.database.reference
+        val listener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if(snapshot.exists()) {
+                    val data = snapshot.getValue(String::class.java)
+                    data?.let { setText("Firebase remote: $it") }
+                } else {
+                    setText("Route without data")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@MainActivity, "An error ocurred", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        dataReference = database.child("initial_firebase").child("data")
+        dataReference.addValueEventListener(listener)
+
+        setListeners()
+    }
+
+    private fun setText(data: String) {
+        _binding.tvData.text = data
+    }
+
+    private fun setListeners() {
+        with(_binding) {
+            btnSend.setOnClickListener {
+                dataReference.setValue(tieData.text.toString())
+            }
+
+            btnSend.setOnLongClickListener {
+                dataReference.removeValue()
+                true
+            }
+        }
     }
 }
